25dde887e3d4930da4772c3b5260b60bb5ef8fcb
Etishagarg <gargetisha@gmail.com>
2025-05-21 01:15:44 +0530

    Initial Commit

---
+++ CuriousApp/CuriousAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuriousAppApp.swift
+//  CuriousApp
+//
+//  Created by Etisha Garg on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CuriousAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
